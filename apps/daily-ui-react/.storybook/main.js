f48aec93d71ddebed984143e49f8f3f55d640bfa
Carlos Santiago <5726971+csantiago132@users.noreply.github.com>
2025-04-26 16:19:23 -0500

    chore(config): syncs deps between projects (#3)

--- apps/daily-ui-react/.storybook/main.js
+++ apps/daily-ui-react/.storybook/main.js
@@ -18,7 +18,7 @@ const config = {
   },
   docs: {},
   typescript: {
-    reactDocgen: 'react-docgen-typescript',
+    reactDocgen: 'daily-ui-react-docgen-typescript',
     reactDocgenTypescriptOptions: {
       shouldExtractLiteralValuesFromEnum: true,
       compilerOptions: {
