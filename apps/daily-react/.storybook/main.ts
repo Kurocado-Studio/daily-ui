04f5a128ff199e993b93fc12848154303a282bfe
Carlos Santiago <5726971+csantiago132@users.noreply.github.com>
2025-05-03 18:06:24 -0500

    fix(config): fix tailwind preset

--- apps/daily-react/.storybook/main.ts
+++ apps/daily-react/.storybook/main.ts
@@ -1,21 +1,23 @@
 /* eslint import/no-default-export: 0 */
-import autoprefixer from 'autoprefixer';
-import tailwindcss from 'tailwindcss';
-import { mergeConfig } from 'vite';
+import { dirname, join } from 'node:path';
+import type { InlineConfig, UserConfig } from 'vite';
 
+function getAbsolutePath(value: string): string {
+  return dirname(require.resolve(join(value, 'package.json')));
+}
 const config = {
   stories: [
     '../stories/**/*.mdx',
     '../stories/**/*.stories.@(js|jsx|mjs|ts|tsx)',
   ],
   addons: [
-    '@storybook/addon-links',
-    '@storybook/addon-essentials',
-    '@storybook/addon-onboarding',
-    '@storybook/addon-interactions',
-    '@chromatic-com/storybook',
-    '@storybook/addon-docs',
-    '@storybook/addon-themes',
+    getAbsolutePath('@storybook/addon-links'),
+    getAbsolutePath('@storybook/addon-essentials'),
+    getAbsolutePath('@storybook/addon-onboarding'),
+    getAbsolutePath('@storybook/addon-interactions'),
+    getAbsolutePath('@chromatic-com/storybook'),
+    getAbsolutePath('@storybook/addon-docs'),
+    getAbsolutePath('@storybook/addon-themes'),
   ],
   framework: {
     name: '@storybook/react-vite',
@@ -32,15 +34,7 @@ const config = {
       },
     },
   },
-  viteFinal: (config) => {
-    mergeConfig(config, {
-      css: {
-        postcss: {
-          plugins: [tailwindcss(), autoprefixer()],
-        },
-      },
-    });
-
+  viteFinal: async (config: InlineConfig): Promise<UserConfig> => {
     config.server = config.server || {};
     config.server.watch = {
       ...config.server.watch,
--- apps/daily-vue/.storybook/main.ts
+++ apps/daily-vue/.storybook/main.ts
@@ -1,5 +1,10 @@
 /* eslint import/no-default-export: 0 */
 import type { StorybookConfig } from '@storybook/vue3-vite';
+import { dirname, join } from 'node:path';
+
+function getAbsolutePath(value: string): string {
+  return dirname(require.resolve(join(value, 'package.json')));
+}
 
 const config: StorybookConfig = {
   stories: [
@@ -7,10 +12,10 @@ const config: StorybookConfig = {
     '../stories/**/*.stories.@(js|jsx|mjs|ts|tsx)',
   ],
   addons: [
-    '@storybook/addon-essentials',
-    '@storybook/addon-onboarding',
-    '@chromatic-com/storybook',
-    '@storybook/experimental-addon-test',
+    getAbsolutePath('@storybook/addon-essentials'),
+    getAbsolutePath('@storybook/addon-onboarding'),
+    getAbsolutePath('@chromatic-com/storybook'),
+    getAbsolutePath('@storybook/experimental-addon-test'),
   ],
   framework: {
     name: '@storybook/vue3-vite',
