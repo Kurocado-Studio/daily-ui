dd618db804f3dbebec9e27fbcdeaee3531c2c70d
Carlos Santiago <5726971+csantiago132@users.noreply.github.com>
2025-05-03 19:01:35 -0500

    fix(ui): adds Vue component

---
+++ apps/daily-vue/src/daily-ui-001/Daily001.vue
@@ -0,0 +1,11 @@
+<template>
+  <Card>
+    <CardBody>
+      <Input />
+    </CardBody>
+  </Card>
+</template>
+
+<script setup lang="ts">
+import { Card, CardBody, Input } from '@kurocado-studio/ui/vue';
+</script>
