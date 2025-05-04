a78815d2d855b9eda408e95045431bb8426d1b7b
Carlos Santiago <5726971+csantiago132@users.noreply.github.com>
2025-05-04 11:52:02 -0500

    fix(ui): resyncs Vue

--- apps/daily-vue/.storybook/main.ts
+++ apps/daily-vue/.storybook/main.ts
@@ -1,21 +1,26 @@
-/* eslint import/no-default-export: 0 */
 import type { StorybookConfig } from '@storybook/vue3-vite';
+import { dirname, join } from 'path';
 
+/**
+ * This function is used to resolve the absolute path of a package.
+ * It is needed in projects that use Yarn PnP or are set up within a monorepo.
+ */
+function getAbsolutePath(value: string): any {
+  return dirname(require.resolve(join(value, 'package.json')));
+}
 const config: StorybookConfig = {
   stories: [
     '../stories/**/*.mdx',
     '../stories/**/*.stories.@(js|jsx|mjs|ts|tsx)',
   ],
   addons: [
-    '@storybook/addon-links',
-    '@storybook/addon-essentials',
-    '@storybook/addon-onboarding',
-    '@storybook/addon-interactions',
-    '@storybook/addon-docs',
-    '@storybook/addon-themes',
+    getAbsolutePath('@storybook/addon-essentials'),
+    getAbsolutePath('@storybook/addon-onboarding'),
+    getAbsolutePath('@chromatic-com/storybook'),
+    getAbsolutePath('@storybook/experimental-addon-test'),
   ],
   framework: {
-    name: '@storybook/vue3-vite',
+    name: getAbsolutePath('@storybook/vue3-vite'),
     options: {},
   },
 };
